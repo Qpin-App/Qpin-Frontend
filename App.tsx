f3f87db513faed0dcbf8b711946ebe705f3075be
권용현 <dydgus9952@naver.com>
2025-03-09 16:02:58 +0900

    feat: bottom tab and basic pages

--- App.tsx
+++ App.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./src/screens/HomeScreen";
+import HomeScreen from "./src/screens/Home/HomeScreen";
+import QrScreen from "./src/screens/Qr/QrScreen";
+import ParkingScreen from "./src/screens/Parking/ParkingScreen";
+import SearchScreen from "./src/screens/Search/SearchScreen";
+import InsuranceScreen from "./src/screens/Insurance/InsuranceScreen";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+// function HomeStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Home" component={HomeScreen} />
+//     </Stack.Navigator>
+//   );
+// }
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Qr" component={QrScreen} />
+        <Tab.Screen name="Parking" component={ParkingScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Insurance" component={InsuranceScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default App;
+}
